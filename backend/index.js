5cf3343169796147b6001fbeb8446df3d0247f9c
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-16 18:45:10 -0700

    fix port issue

--- backend/index.js
+++ backend/index.js
@@ -98,14 +98,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
