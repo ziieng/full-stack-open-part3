a3938d4852e8b48bd74f03a413c2934571110df9
Zii Engelhardt <ziiengelhardt@gmail.com>
2025-03-26 15:46:56 -0700

    exercise 3.12 - command line mongodb connection

---
+++ backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+// combined error handling for arg lengths
+if (process.argv.length < 3 || process.argv.length === 4 || process.argv.length > 5) {
+  console.log('Invalid arguments!')
+  console.log('* To list current contacts, run "node mongo.js <password>"')
+  console.log('* To add a new contact, run "node mongo.js <password> <name> <number>"')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://ziiengelhardt:${password}@cluster0.juzlo1u.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if(process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
